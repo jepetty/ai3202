ae78f05d9ea6a276cb3a24ef16512f705438556d
Jessica Petty <jessica.petty@colorado.edu>
2015-09-17 13:04:44 -0600

    Create

---
+++ Assigment2/AStarSearch.py
@@ -0,0 +1,22 @@
+# Jessica Petty
+# CSCI 3202
+# Assignment 2
+# September 18, 2015
+
+## Command line argument to read in files
+
+## Command line argument to depict which heuristic we're using
+
+## Calculate optimal path using AStar
+
+## Output
+	# The cost of the path
+	# The locations along the path
+	# The number of locations evaluated
+	
+# Create nodes to add to a list
+	# Location: (int, int)
+	# distanceToStart: int
+	# heuristic: int
+	# f: int
+	# parent: Node
