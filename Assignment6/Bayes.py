c2f38f5e4062f31bb977c493bdc93144dd0a7518
jepetty <jessica.pet70@gmail.com>
2015-10-20 15:00:22 -0600

    Created node class in Bayes, implemented getopt

--- Assignment6/Bayes.py
+++ Assignment6/Bayes.py
@@ -6,3 +6,32 @@
 import getopt
 import sys
 
+# Define node class to represent each node in the Bayes Net
+class Node:
+	def __init__(self):
+		self.marginal = 0
+		self.conditionals = {}
+		self.name = ""
+
+# Fetch arguments, options from command line
+opts, args = getopt.getopt(sys.argv[1:], 'm:j:p:g')
+# Evaluate options, call appropriate function
+for opt in opts:
+	if opt[0] == '-m':
+		arg = opt[1]
+		# call marginal probability function
+	elif opt[0] == '-j':
+		arg = opt[1]
+		# call joint probability function
+	elif opt[0] == '-p':
+		arg = opt[1]
+		probability = sys.argv[2]
+		# call set prior function for smoking or pollution
+	elif opt[0] == '-g':
+		arg = opt[1]
+		# call conditional probability function
+
+# Function to parse the argument from option list
+def parseArgument(arg):
+	# Parse the argument 
+	print(arg)
