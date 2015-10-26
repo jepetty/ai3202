dcb2194d14bacade8627174acbb7d6df01181a16
jepetty <jessica.pet70@gmail.com>
2015-10-26 10:16:14 -0600

    Implemented parser for arguments to command line

--- Assignment6/Bayes.py
+++ Assignment6/Bayes.py
@@ -12,26 +12,53 @@ class Node:
 		self.marginal = 0
 		self.conditionals = {}
 		self.name = ""
-
-# Fetch arguments, options from command line
-opts, args = getopt.getopt(sys.argv[1:], 'm:j:p:g')
-# Evaluate options, call appropriate function
-for opt in opts:
-	if opt[0] == '-m':
-		arg = opt[1]
-		# call marginal probability function
-	elif opt[0] == '-j':
-		arg = opt[1]
-		# call joint probability function
-	elif opt[0] == '-p':
-		arg = opt[1]
-		probability = sys.argv[2]
-		# call set prior function for smoking or pollution
-	elif opt[0] == '-g':
-		arg = opt[1]
-		# call conditional probability function
+		self.parents = None
+		self.children = None
 
 # Function to parse the argument from option list
 def parseArgument(arg):
 	# Parse the argument 
 	print(arg)
+	
+# Main function to receive arguments, begin processing them
+def main():
+    try:
+        opts, args = getopt.getopt(sys.argv[1:], "m:g:j:p:")
+    except getopt.GetoptError as err:
+        # print help information and exit:
+        print(err) # will print something like "option -a not recognized"
+        sys.exit(2)
+    for o, a in opts:
+        if o in ("-p"):
+            print("flag: ", o)
+            print("args: ", a)
+            print(a[0])
+            print(float(a[1:]))
+			#setting the prior here works if the Bayes net is already built
+			#setPrior(a[0], float(a[1:])
+        elif o in ("-m"):
+            print("flag: ", o)
+            print("args: ", a)
+            print(type(a))
+			#calcMarginal(a)
+        elif o in ("-g"):
+            print("flag: ", o)
+            print("args: ", a)
+            print(type(a))
+			# you may want to parse a here and pass the left of |
+			# and right of | as arguments to calcConditional
+            p = a.find("|")
+            print(a[:p])
+            print(a[p+1:])
+			#calcConditional(a[:p], a[p+1:])
+        elif o in ("-j"):
+            print("flag: ", o)
+            print("args: ", a)
+        else:
+            assert False, "unhandled option"
+		
+    # ...
+
+if __name__ == "__main__":
+    main()
+
