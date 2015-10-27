5efbaf3e8636e9354520e2a8db5d3e80cc7de70d
jepetty <jessica.pet70@gmail.com>
2015-10-27 14:04:23 -0600

    Implemented calculations of joint probabilities and distributions

--- Assignment6/Bayes.py
+++ Assignment6/Bayes.py
@@ -23,11 +23,11 @@ def createNetwork():
 	xrayNode = Node()
 	dyspnoeaNode = Node()
 	
-	pollutionNode.name = "pollution"
-	smokerNode.name = "smoker"
-	cancerNode.name = "cancer"
-	xrayNode.name = "xray"
-	dyspnoeaNode.name = "dyspnoea"
+	pollutionNode.name = "p"
+	smokerNode.name = "s"
+	cancerNode.name = "c"
+	xrayNode.name = "x"
+	dyspnoeaNode.name = "d"
 	
 	pollutionNode.children.append(cancerNode)
 	smokerNode.children.append(cancerNode)
@@ -65,6 +65,9 @@ def setPrior(network, arg, value):
 		print("Cannot set the prior for this variable")
 	
 def calcMarginal(network, arg):
+	if arg[0] == '~':
+		marginal = calcMarginal(network, arg[1])
+		return ("Not " + marginal[0], 1 - marginal[1])
 	if arg == "P" or arg == "p":
 		node = network["pollution"]
 		return ("Pollution", node.marginal)
@@ -88,14 +91,80 @@ def calcMarginal(network, arg):
 		node = network["dyspnoea"]
 		conditionals = node.conditionals
 		cancerMarginal = calcMarginal(network, "c")
-		marginal = conditionals["C"]*cancerMarginal[1] + conditionals["~c"]*(1-cancerMarginal[1])
+		marginal = conditionals["c"]*cancerMarginal[1] + conditionals["~c"]*(1-cancerMarginal[1])
 		return ("Dyspnoea", marginal)
 	else:
 		print("Requesting marginal distribution for an invalid variable")
 
+# Helper function to parse the variables included in the joint probability
+def parseJoint(arg):
+	jointVarList = []
+	newArg = ""
+	argLength = len(arg)
+	for i in range(0, argLength):
+		if arg[i] == '~':
+			i = i + 1
+		newArg = newArg + arg[i].lower()
+	length = len(newArg)
+	for i in range(0, length):
+		newVar = []
+		if len(jointVarList) == 0:
+			newVar.append(newArg[i])
+			newVar.append("~" + newArg[i])
+		else: 
+		    for var in jointVarList:
+			    newVar.append(var + newArg[i])
+			    newVar.append(var + "~" + newArg[i])
+		jointVarList = newVar
+		i = i + 1
+	return jointVarList
+
+# Function to calculate and return the joint probability
+def calcJointDistribution(network, arg):
+	variableList = parseJoint(arg)
+	probabilities = {}
+	if arg in variableList:
+		return calcJointProbability(network, arg)
+	else:
+	    for var in variableList: 
+		    probabilities[var] = calcJointProbability(network, var)
+	    return probabilities
+
+# Function to calculate the probability for individual, specific joint cases
+def calcJointProbability(network, arg):
+	probability = 1
+	length = len(arg)
+	for i in range(0, length):
+		if arg[i] == "~":
+			i = i + 1
+			newArg = "~" + arg[i]
+			probability = probability * calcConditional(network, newArg, arg[i+1:])
+		else:
+			probability = probability * calcConditional(network, arg[i], arg[i+1:])
+		i = i + 1
+	return probability	
+
+# Function to parse variables for conditional probability function
+def parseVariables(variables):
+	varList = []
+	length = len(variables)
+	for i in range(0, length):
+		if variables[i] == '~':
+			i = i + 1
+			var = variables[i-1] + variables[i]
+			varList.append(var)
+		else:
+			varList.append(variables[i])
+		i = i + 1
+	return varList
+
 # Function to calculate the conditional probability
 def calcConditional(network, arg, con):
-	conLength = len(con)
+	conList = parseVariables(con)
+	notBool = False
+	if arg[0] == '~':
+		notBool = True
+		arg = arg[1]
 	if arg == "p":
 		node = network["pollution"]
 	elif arg == "s":
@@ -116,7 +185,8 @@ def calcConditional(network, arg, con):
 			conditional = node.conditionals[con]
 			return("Dyspnoea", conditional)
 	else:
-		print("Requestion marginal distribution for an invalid variable")
+		print("Requesting conditional distribution for an invalid variable: ", arg)
+	return 1
 			
 # Main function to receive arguments, begin processing them
 def main():
@@ -152,7 +222,7 @@ def main():
         elif o in ("-j"):
             print("flag: ", o)
             print("args: ", a)
-            #calcJoint(o,a)
+            calcJointDistribution(bayesNetwork, a)
         else:
             assert False, "unhandled option"
 		
